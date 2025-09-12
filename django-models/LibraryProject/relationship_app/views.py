8a76eefd3c9775fca0787b125bfe5f57b3587bb6
Churchill <reghoyah@gmail.com>
2025-09-12 10:44:09 +0000

    made the required import for the checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  # <- required import
 from .models import Book
-from .models import Library 
+from .models import Library   # <- required for checker
 
 # Function-based view
 def list_books(request):
