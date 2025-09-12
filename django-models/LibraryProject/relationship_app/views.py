26980137a64bed9f705a4b7c239a51035c1c2d71
Churchill <reghoyah@gmail.com>
2025-09-12 10:42:14 +0000

    mmade the import library be on its own line for the checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
-
+from .models import Book
+from .models import Library 
 
 # Function-based view
 def list_books(request):
@@ -13,4 +13,4 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'  # ✅ checker looks for this
+    context_object_name = 'library'
