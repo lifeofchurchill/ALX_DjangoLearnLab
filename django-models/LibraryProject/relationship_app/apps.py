d618ddefeffa546f5c879e32e5930a7be6234d4e
Churchill <reghoyah@gmail.com>
2025-09-12 10:32:17 +0000

    trying to find where the problem is

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    # Fetch all books
     books = Book.objects.all()
-    
-    # Pass them to the template
-    context = {'book_list': books}
-    
-    return render(request, "relationship_app/list_books.html", context)
+    return render(request, "relationship_app/list_books.html", {"books": books})
