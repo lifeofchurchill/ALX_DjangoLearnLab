60ffc1c6c3feb03c053b0660579b9ac22f66b456
Churchill <reghoyah@gmail.com>
2025-09-12 10:36:02 +0000

    finally found the issue didnt add code to the views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    # Fetch all books
+    books = Book.objects.all()
+    
+    # Pass them to the template
+    context = {'book_list': books}
+    
+    return render(request, "relationship_app/list_books.html", context)
