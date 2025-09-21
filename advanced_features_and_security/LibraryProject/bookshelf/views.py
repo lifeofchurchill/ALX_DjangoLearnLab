98ee14b9e557c02028ab9f0b4a316f3221bf97df
Churchill <reghoyah@gmail.com>
2025-09-21 22:34:37 +0000

    made changes to the view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,16 +1,6 @@
-from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from .models import Book
 
-@permission_required('relationship_app.can_create', raise_exception=True)
-def add_book(request):
-    # logic to add book
-    pass
-
-@permission_required('relationship_app.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    # logic to edit book
-    pass
-
-@permission_required('relationship_app.can_delete', raise_exception=True)
-def delete_book(request, pk):
-    # logic to delete book
-    pass
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
